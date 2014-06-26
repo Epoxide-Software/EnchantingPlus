d90f0bce19caacc07498e256e14ac3f954cb9096
freyjadono <mike.stengel@gmail.com>
2014-06-26 07:15:58 -0400

    API Jar

--- src/main/java/com/aesireanempire/eplus/api/package-info.java
+++ src/main/java/com/aesireanempire/eplus/api/package-info.java
@@ -1,4 +1,8 @@
 /**
  * Created by freyja on 6/26/14.
  */
+@API(owner = References.MODID, apiVersion = "1.0", provides = "eplus|api")
 package com.aesireanempire.eplus.api;
+
+import com.aesireanempire.eplus.lib.References;
+import cpw.mods.fml.common.API;
